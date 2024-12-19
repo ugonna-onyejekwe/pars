4158377e530d80e3aecf83655f63a3c87123b200
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-12-19 09:35:21 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">ugonna</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
